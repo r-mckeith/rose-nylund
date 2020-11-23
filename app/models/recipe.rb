eaf09de433e98d1eb0a40d1d744fdc571e10cd2f
rmmckeith <r.mckeith@me.com>
2020-11-23 15:14:31 -0800

    build tests and validations for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimim: 5, maximum: 500 }
 end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+
+  def setup
+    @recipe = Recipe.new(name: "Tofu", description: "Is really tasty and good")
+  end
+
+  test "recipe should be valid" do 
+    assert @recipe.valid?
+  end
+
+  test "name should be present" do
+    assert @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be present" do
+    assert @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+
+  test "description should be at least 5 characters" do
+    @recipe.description = "a" * 3
+    assert_not @recipe.valid?
+  end
+
+  test "description should be no more than 500 characters" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+end
