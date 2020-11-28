4fc6094d9658c7e4f299b21eeb5dafe8728a2d54
rmmckeith <r.mckeith@me.com>
2020-11-27 17:07:51 -0800

    create edit recipe view and action

---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: "Ryan", email: "ryan@example.com")
+    @recipe = @chef.recipes.create(name: "Vegetable saute", description: "very tasty and good.")
+  end
+
+  test "a valid recipe is updated" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_description = "updated recipe description"
+    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_description } }
+    assert_redirected_to @recipe
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_description, @recipe.description
+  end
+
+  test "an invalid recipe is not updated" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", dewcription: "some description" } }
+    assert_template 'recipes/edit'
+    assert_select 'div.panel-body'
+  end
+     
+end
