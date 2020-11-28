e815240a4fc6adb51373198b53dc282d1a31d662
rmmckeith <r.mckeith@me.com>
2020-11-28 10:46:17 -0800

    create new user function

--- test/integration/chefs_signup_test.rb
+++ test/integration/chefs_signup_test.rb
@@ -8,9 +8,24 @@ class ChefsSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "reject invalid signup" do
+    get signup_path
+    assert_no_difference "Chef.count" do
+      post chefs_path, params: { chef: { chefname: " ", email: " ", password: " ",
+                                 password_confirmation: " " } }
+    end
+    assert_template 'chefs/new'
+    assert_select 'div.panel-body'
   end
 
   test "accept valid signup" do
+    get signup_path
+    assert_difference "Chef.count", 1 do
+      post chefs_path, params: { chef: { chefname: "Ryan", email: "ryan@example.com", password: "password",
+                                 password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'chefs/show'
+    assert_not flash.empty?
   end
 
 end
