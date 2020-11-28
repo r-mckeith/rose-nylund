2b1ab87c0102df10db89a4d2e3dd30f289ef8bf7
rmmckeith <r.mckeith@me.com>
2020-11-28 10:23:19 -0800

    create new user form

---
+++ test/integration/chefs_signup_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ChefsSignupTest < ActionDispatch::IntegrationTest
+  
+  test "should get signup path" do
+    get signup_path
+    assert_response :success
+  end
+
+  test "reject invalid signup" do
+  end
+
+  test "accept valid signup" do
+  end
+
+end
