5b73ab3ef4f33918cd171d91f4ad0db3bc98b9c6
rmmckeith <r.mckeith@me.com>
2020-11-30 10:15:18 -0800

    update tests to account  for authentication

--- test/integration/chefs_show_test.rb
+++ test/integration/chefs_show_test.rb
@@ -15,6 +15,7 @@ class ChefsShowTest < ActionDispatch::IntegrationTest
   end
 
   test "should delete chef" do
+    sign_in_as(@chef, "password")
     get chefs_path
     assert_template 'chefs/index'
     assert_difference 'Chef.count', -1 do
