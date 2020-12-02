85fc6b5146c8b350dd97ffb32a4a181998375928
rmmckeith <r.mckeith@me.com>
2020-12-01 22:10:58 -0800

    add controller restrictions

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -8,6 +8,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "reject invalid edit" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: " ", email: "ryan@example.com" } }
@@ -16,6 +17,7 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   end
 
   test "accept valid edit" do
+    sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
     patch chef_path(@chef), params: { chef: { chefname: "ryan1", email: "ryan1@example.com" } }
--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -9,6 +9,7 @@ class RecipesEditTest < ActionDispatch::IntegrationTest
   end
 
   test "a valid recipe is updated" do
+    sign_in_as(@chef, "password")
     get edit_recipe_path(@recipe)
     assert_template 'recipes/edit'
     updated_name = "updated recipe name"
@@ -22,6 +23,7 @@ class RecipesEditTest < ActionDispatch::IntegrationTest
   end
 
   test "an invalid recipe is not updated" do
+    sign_in_as(@chef, "password")
     get edit_recipe_path(@recipe)
     assert_template 'recipes/edit'
     patch recipe_path(@recipe), params: { recipe: { name: " ", dewcription: "some description" } }
